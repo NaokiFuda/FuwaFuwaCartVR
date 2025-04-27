9b7d7c99aed3ba50c8fa223bf27d164ca0070ae9
gizmo-pants <gigis.coutings.quad@gmail.com>
2025-04-27 22:04:08 +0900

    各ボーンの位置の計算をまともにした。回転に対してどれだけ元の位置に居残るのか？という関数とバネ的な動きの関数を同期的に（waitとかしない）方針を固めた。動きは現状罪悪。なんか一方向常に向いてる。要改善。

--- Assets/Scripts/FuwaFuwaMovement.cs
+++ Assets/Scripts/FuwaFuwaMovement.cs
@@ -8,58 +8,57 @@ public class FuwaFuwaMovement : MonoBehaviour
     [SerializeField]Transform rootBone;
     Transform[] bonesTransform;
     float[] bonesLength;
+    
     void Start()
     {
-        if(rootBone == null) rootBone = transform;
+        if (rootBone == null) { rootBone = transform;}
         bonesTransform = rootBone.transform.GetComponentsInChildren<Transform>();
-        bonesLength = new float[bonesLength.Length];
-        var parentTransform = rootBone.transform;
+        bonesLength = new float[bonesTransform.Length];
         for(int i=1; i< bonesTransform.Length;i++ )
         {
             var t = bonesTransform[i];
-            var startPos = parentTransform.position;
             var k = 1;
-            if (parentTransform != t.parent)
-            {
-                parentTransform = t.parent; 
-                startPos = bonesTransform[i - 1].position; 
-            }
-            else 
-            {
-                startPos = parentTransform.position;
-                while(bonesTransform[i-k] != parentTransform) k++;
-            }
-            bonesLength[i] = bonesLength[i - k] + Vector3.Distance(startPos, t.position);
+            while (bonesTransform[i - k] != t.parent) k++;
+            bonesLength[i] = bonesLength[i - k] + Vector3.Distance(t.parent.position, t.position);
         }
     }
     Vector3 lastPos;
     Vector3 lastDir;
     void Update()
     {
-        //CaluculateFuwafuwa( CaluculateSwingDirection() );
-    }
-    Vector3 CaluculateSwingDirection()
-    {
-        var swingDir = rootBone.position - lastPos;
-        swingDir += (rootBone.forward - lastDir).normalized * Vector3.Angle(lastDir, rootBone.forward);
-        
+        for(int i= 0; i< bonesTransform.Length;i++)
+            DoFuwa( CaluculateSwingDirection(i) );
+
         lastPos = rootBone.position;
-        lastDir = rootBone.forward;
-        return -swingDir;
+        lastDir = rootBone.up;
     }
-    void CaluculateFuwafuwa(Vector3 forceDir)
+    Vector4 CaluculateSwingDirection( int i )
     {
-        for (int i = 1; i < bonesTransform.Length; i++)
-        {
-            var t = bonesTransform[i];
-            DoFuwafuwa(forceDir);
-        }
+        Vector3 swingDir = rootBone.position - lastPos;
+        swingDir += (rootBone.up - lastDir) * bonesLength[i];
+        swingDir /= 2;
+        Vector4 swingDir4 = -swingDir;
+        swingDir4.w = i;
+
+        return swingDir4;
     }
-    async void DoFuwafuwa(Vector3 forceDir)
+    [SerializeField] float bounceness;
+    [SerializeField] float hardness = 0.1f;
+    void DoFuwa(Vector4 swingDir4)
+    {
+        Vector3 forceDir = swingDir4;
+        var i = (int)swingDir4.w;
+        var stiffness = hardness - bonesLength[i];
+        bonesTransform[i].rotation = Quaternion.Slerp(bonesTransform[i].rotation, Quaternion.FromToRotation(bonesTransform[i].up, bonesTransform[i].up+forceDir), 1 - stiffness);
+    }
+    void ReFuwa(Vector4 swingDir4)
     {
 
+    }
+    async void DelayFuwa()
+    {
         await Task.Delay(1);
-        await Task.Run(() => DoFuwafuwa(forceDir));
+        await Task.Run(() => DelayFuwa());
     }
 }
 
