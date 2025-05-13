9f0928c70b5f97b91a8895f38f8b459c142b86c5
gizmopants <naokifuda@gmail.com>
2025-05-13 17:36:50 +0900

    なんかVR用設定やスクリプトの調節をした

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -10,8 +10,10 @@ using UnityEngine.Events;
 
 public class PlayerControler : MonoBehaviour
 {
-    Action moveMethod;
+    Action<Vector3> moveMethod;
     Action jumpMethod;
+    [SerializeField] Transform playerLeftHand;
+    [SerializeField] Transform playerRightHand;
 
     public SteamVR_Action_Boolean _moveForward = SteamVR_Input.GetBooleanAction("MoveForward");
     public SteamVR_Action_Boolean _moveBackward = SteamVR_Input.GetBooleanAction("MoveBackward");
@@ -39,70 +41,90 @@ public class PlayerControler : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         player = GetComponent<Player>();
         col = GetComponent<CapsuleCollider>();
+        playerLeftHand.parent = player.leftHand.transform;
+        playerRightHand.parent = player.rightHand.transform;
     }
     void Update()
     {
-        if (XRSettings.isDeviceActive) Debug.Log("Yes");
-        else Debug.Log("no");
-        Vector2 input = _moveAction.GetAxis(SteamVR_Input_Sources.LeftHand);
+        bool isJump = false;
+        bool rightTurnInput = false;
+        bool leftTurnInput = false;
+        Vector2 turnInput = Vector2.zero;
 
-        bool forwardInput = _moveForward.GetStateDown(SteamVR_Input_Sources.LeftHand);
-        bool backwardInput = _moveBackward.GetStateDown(SteamVR_Input_Sources.LeftHand);
-        bool rightwardInput = _moveRightward.GetStateDown(SteamVR_Input_Sources.LeftHand);
-        bool leftwardInput = _moveLeftward.GetStateDown(SteamVR_Input_Sources.LeftHand);
-
-        if (forwardInput) moveDirection = player.hmdTransform.forward;
-
-        else if (backwardInput) moveDirection = -player.hmdTransform.forward;
-
-        else if (rightwardInput) moveDirection = player.hmdTransform.right;
-
-        else if (leftwardInput) moveDirection = -player.hmdTransform.right;
-
-        else if (input.x != 0 || input.y != 0)
+        if (XRSettings.isDeviceActive)
         {
-            moveDirection = player.hmdTransform.forward * input.y;
-            moveDirection += player.hmdTransform.right * input.x;
-        }
-        else moveDirection = Vector3.zero;
 
-        moveDirection.y = 0;
+            Vector2 input = _moveAction.GetAxis(SteamVR_Input_Sources.LeftHand);
+
+            bool forwardInput = _moveForward.GetStateDown(SteamVR_Input_Sources.LeftHand);
+            bool backwardInput = _moveBackward.GetStateDown(SteamVR_Input_Sources.LeftHand);
+            bool rightwardInput = _moveRightward.GetStateDown(SteamVR_Input_Sources.LeftHand);
+            bool leftwardInput = _moveLeftward.GetStateDown(SteamVR_Input_Sources.LeftHand);
+
+            if (forwardInput) moveDirection = player.hmdTransform.forward;
+
+            else if (backwardInput) moveDirection = -player.hmdTransform.forward;
+
+            else if (rightwardInput) moveDirection = player.hmdTransform.right;
+
+            else if (leftwardInput) moveDirection = -player.hmdTransform.right;
+
+            else if (input.x != 0 || input.y != 0)
+            {
+                moveDirection = player.hmdTransform.forward * input.y;
+                moveDirection += player.hmdTransform.right * input.x;
+            }
+            else moveDirection = Vector3.zero;
+
+            moveDirection.y = 0;
+            
+
+
+            rightTurnInput = _turnRight.GetState(SteamVR_Input_Sources.RightHand);
+            leftTurnInput = _turnLeft.GetState(SteamVR_Input_Sources.RightHand);
+
+            isJump = _jump.GetStateDown(SteamVR_Input_Sources.RightHand);
+            turnInput = _turnAction.GetAxis(SteamVR_Input_Sources.RightHand);
+            
+            
+            if (isFootFollowHead && Mathf.Abs(player.hmdTransform.rotation.x) < leanAngle && Mathf.Abs(player.hmdTransform.rotation.z) < leanAngle)
+            {
+                var hPos = player.hmdTransform.localPosition;
+                col.center = new Vector3(hPos.x, rb.centerOfMass.y, hPos.z);
+            }
+        }
+        var verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+        if (verticalInput != 0) moveDirection.z = verticalInput;
+        if(horizontalInput != 0) moveDirection.x += horizontalInput;
+
+        var mouseMovement = Input.GetAxis("Mouse X");
+        if (mouseMovement != 0) Turn(mouseMovement * mouseSensitivity);
+
         if (moveDirection != Vector3.zero) isMove = true;
         else isMove = false;
-
-
-        bool rightTurnInput = _turnRight.GetState(SteamVR_Input_Sources.RightHand);
-        bool leftTurnInput = _turnLeft.GetState(SteamVR_Input_Sources.RightHand);
-
-        bool isJump = _jump.GetStateDown(SteamVR_Input_Sources.RightHand);
-        Vector2 turnInput = _turnAction.GetAxis(SteamVR_Input_Sources.RightHand);
         if (isJump)
         {
-            // ƒWƒƒƒ“ƒvˆ—
+            jumpMethod?.Invoke();
         }
-        if (rightTurnInput|| leftTurnInput)
+        if (rightTurnInput || leftTurnInput)
         {
             Turn(turnInput.x);
         }
-        if (isFootFollowHead && Mathf.Abs(player.hmdTransform.rotation.x) < leanAngle && Mathf.Abs(player.hmdTransform.rotation.z) < leanAngle)
-        {
-            var hPos = player.hmdTransform.localPosition;
-            col.center = new Vector3(hPos.x, rb.centerOfMass.y, hPos.z);
-        }
-       
     }
     Vector3 moveDirection;
     bool isMove;
+    [SerializeField] float mouseSensitivity = 0.03f;
 
     private void FixedUpdate()
     {
         if (isMove)
         {
-            jumpMethod?.Invoke();
+            moveMethod?.Invoke(moveDirection);
         }
     }
     private void Turn(float angle)
     {
-        moveMethod?.Invoke();
+        player.transform.RotateAround(player.hmdTransform.position, Vector3.up, angle);
     }
 }
